ae08da63213472287da9d865156df628806e717f
Alexandra Girard <alexgirard999@gmail.com>
2018-09-18 00:52:09 -0400

    basic desc

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+
+import Title from "./components/Title";
+
+class App extends React.Component {
+  render() {
+    return (
+        <div>
+          <Title />
+        </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/Title.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Title = () => (
+  <div>
+    <p><bold>alexandra</bold></p>
+    <p><bold>girard</bold></p>
+    <p className="normal">
+      Hi there! My name’s Alex
+      and I’m currently a software
+      engineering student
+      at the University of Waterloo.
+    </p>
+  </div>
+);
+
+
+export default Title;
